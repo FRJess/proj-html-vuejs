c7a974c8565c9fcb5e9d14d1d9a538d392be94af
FR_Jess <fr_jess@icloud.com>
2022-11-24 12:55:01 +0100

    [import] fonts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script>
+import TheHeader from './components/TheHeader.vue'
+import AppMain from './components/AppMain.vue'
+import TheFooter from './components/TheFooter.vue'
+
+
 export default {
-  name:'App'
+  name:'App',
+
+  components:{
+    TheHeader,
+    AppMain,
+    TheFooter,
+  }
 
 }
 </script>
 
 <template>
+
+  <TheHeader/>
+  <AppMain/>
+  <TheFooter/>
   
 </template>
 
